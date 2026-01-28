ce89912cdf9868f55b010b331f9f452457c49e01
vadimartynov <vadim.martynov@flant.com>
2026-01-28 19:05:55 +0300

    Fixed cleanupStorage
    Signed-off-by: vadimartynov <vadim.martynov@flant.com>

--- registry/proxy/proxyregistry.go
+++ registry/proxy/proxyregistry.go
@@ -403,23 +403,41 @@ func cleanupNonCacheStorage(ctx context.Context, storage driver.StorageDriver) e
 	return nil
 }
 
+func cleanupStorage(ctx context.Context, storage driver.StorageDriver) error {
+	if err := pathDelete(ctx, storage, schedulerStateFilePath); err != nil {
+		return err
+	}
+	// registry/storage/paths.go:13
+	if err := pathDelete(ctx, storage, "/docker"); err != nil {
+		return err
+	}
+	return nil
+}
+
 func scheduleStateExists(ctx context.Context, storage driver.StorageDriver) (bool, error) {
-	_, err := storage.Stat(ctx, schedulerStateFilePath)
+	return pathExists(ctx, storage, schedulerStateFilePath)
+}
+
+func pathExists(ctx context.Context, storage driver.StorageDriver, path string) (bool, error) {
+	_, err := storage.Stat(ctx, path)
 	switch {
 	case err == nil:
 		return true, nil
 	case errors.As(err, &driver.PathNotFoundError{}):
 		return false, nil
 	default:
-		return false, fmt.Errorf("stat %q: %w", schedulerStateFilePath, err)
+		return false, fmt.Errorf("stat %q: %w", path, err)
 	}
 }
 
-func cleanupStorage(ctx context.Context, storage driver.StorageDriver) error {
-	if err := storage.Delete(ctx, "/"); err != nil {
-		if !errors.As(err, &driver.PathNotFoundError{}) {
-			return fmt.Errorf("cleanup storage: %w", err)
-		}
+func pathDelete(ctx context.Context, storage driver.StorageDriver, path string) error {
+	err := storage.Delete(ctx, path)
+	switch {
+	case err == nil:
+		return nil
+	case errors.As(err, &driver.PathNotFoundError{}):
+		return nil
+	default:
+		return fmt.Errorf("delete %q: %w", path, err)
 	}
-	return nil
 }
